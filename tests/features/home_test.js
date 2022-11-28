a9f82d356904c10b694b7a0414ff448f6baa01e9
jennajeh <jennajuneh@gmail.com>
2022-11-28 23:39:48 +0900

    E2E 테스트 작성 (인수 테스트)

--- tests/features/home_test.js
+++ tests/features/home_test.js
@@ -1,9 +1,27 @@
-Feature('Home page');
+// Feature('홈 화면 - 고객은 서비스를 이용하기 위해 홈 화면에서 로그인 할 수 있다.');
 
-Scenario('Visit the home page', ({ I }) => {
-  // When
-  I.amOnPage('/');
+// Scenario('로그인 하지 않고 홈 화면에 접속한 경우', ({ I }) => {
+//   // When
+//   I.amOnPage('/');
 
-  // then
-  I.see('Hello, world!');
-});
+//   // then
+//   I.see('회원가입');
+//   I.see('로그인');
+// });
+
+// Scenario('로그인 후 홈 화면에 접속한 경우', ({ I }) => {
+//   I.setupDatabase();
+
+//   // When
+//   I.amOnPage('/');
+
+//   I.click('로그인');
+
+//   // then
+//   I.fillField('아이디', 'hello');
+//   I.fillField('비밀번호', 'Testing1!');
+//   I.click('[type=submit]');
+
+//   I.see('내 잔액: 50,000원');
+//   I.see('로그아웃');
+// });
---
+++ tests/features/logout_test.js
@@ -0,0 +1,23 @@
+// Feature('로그아웃');
+
+// Before(({ I }) => {
+//   I.setupDatabase();
+// });
+
+// Scenario('로그아웃', ({ I }) => {
+//   // Given
+//   I.login('Test1');
+
+//   // When
+//   I.amOnPage('/');
+
+//   I.see('내 잔액: 50,000원');
+
+//   I.click('로그아웃');
+
+//   // Then
+//   I.see('회원가입');
+//   I.see('로그인');
+//   I.dontSee('내 잔액: 50,000원');
+//   I.dontSee('로그아웃');
+// });
